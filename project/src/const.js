c03d95eaed9e5ac6aa1f75f48b323c13cad6dfc5
Alexcrudu <60149530+Alexcrudu@users.noreply.github.com>
2021-06-13 14:51:10 +0200

    react-route

---
+++ project/src/const.js
@@ -0,0 +1,8 @@
+export const AppRoute = {
+  MAIN: '/',
+  SIGN_IN: '/login',
+  MY_LIST: '/myList',
+  FILM_DETAILS: '/film/:id',
+  ADD_COMMENT: '/film/:id/review',
+  FILM_PLAYER: '/player/:id',
+};
